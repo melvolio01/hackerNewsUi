79aadbf4c0cf3d95d9ffdab99841e7839352b8a1
Andy Hunt <andyhunt353@gmail.com>
2020-06-16 18:08:25 +0100

    Add hooks to Articles component

--- app/components/Articles.jsx
+++ app/components/Articles.jsx
@@ -1,61 +1,57 @@
-import React, { Component } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { fetchPostIds } from '../utils/api';
 import { removeDuds } from '../utils/helpers';
 import ArticleInfo from './ArticleInfo';
 import ErrorBoundary from './ErrorBoundary';
 import PropTypes from 'prop-types';
 
-class Articles extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            posts: [],
-            loading: true
-        }
-    }
-
-    componentDidMount() {
-        this.fetchPosts()
-    }
-
-
-    componentDidUpdate(prevProps) {
-        const { type } = this.props;
-        if (type !== prevProps.type) {
-            this.fetchPosts()
-        }
-    }
-
-    fetchPosts() {
-        this.setState({
-            posts: [],
-            loading: true
-        })
-
-        fetchPostIds(this.props.type)
-            .then((res) => removeDuds(res))
-            .then((items) => this.setState({
-                posts: items,
+const articleReducer = (state, action) => {
+    switch (action.type) {
+        case 'requesting':
+            return {
+                ...state,
+                posts: [],
+                loading: true
+            }
+        case 'success':
+            return {
+                ...state,
+                posts: action.posts,
                 loading: false
-            }))
-            .catch((err) => console.log(err));
+            }
+        case 'error':
+            return {
+                ...state,
+                error: action.error.message
+            }
     }
+}
 
-    render() {
-        const { posts, loading } = this.state;
-        return (
-            <div className="articles">
-                {loading && (<div className="spinner"></div>)}
-                {posts.map((post) => {
-                    return <div key={post.id} >
-                        <ErrorBoundary>
-                            <ArticleInfo post={post} />
-                        </ErrorBoundary>
-                    </div>
-                })}
-            </div>
-        );
-    }
+const Articles = ({ type }) => {
+    const [state, dispatch] = useReducer(articleReducer,
+        { posts: [], loading: true, error: null })
+
+    useEffect(() => {
+        dispatch({ type: 'requesting' })
+        fetchPostIds(type)
+            .then((res) => removeDuds(res))
+            .then((items) => dispatch({ type: 'success', posts: items }))
+            .catch((err) => dispatch({ type: 'error', error: err.message }))
+    }, [type])
+
+    const { posts, loading } = state;
+    return (
+        <div className="articles">
+            {loading && (<div className="spinner"></div>)}
+            {posts.map((post) => {
+                return <div key={post.id} >
+                    <ErrorBoundary>
+                        <ArticleInfo post={post} />
+                    </ErrorBoundary>
+                </div>
+            })}
+        </div>
+    );
 }
 
 Articles.propTypes = {
