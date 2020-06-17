52138165224f56ee5d81606fdfad52fb22a88c27
Andy Hunt <andyhunt353@gmail.com>
2020-06-17 12:02:04 +0100

    Refactor Author to use hooks

--- app/components/AuthorInfo.jsx
+++ app/components/AuthorInfo.jsx
@@ -1,63 +1,75 @@
-import React, { Component } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { fetchUser, fetchPosts } from '../utils/api';
 import { formatTimestamp } from '../utils/helpers';
 import ArticleInfo from './ArticleInfo';
-import PropTypes from 'prop-types';
 
-class AuthorInfo extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            author: null,
-            posts: null,
-            loading: true
-        }
-    }
-
-    componentDidMount() {
-        const { location: { state: { author } } } = this.props;
-        fetchUser(author)
-            .then(author => this.setState({
-                author: author,
-            }))
-            .then(() => fetchPosts(this.state.author.submitted))
-            .then((posts) => posts.filter(({ type }) => type === 'story'))
-            .then((posts) => this.setState({
-                posts: posts,
+const authorReducer = (state, action) => {
+    switch (action.type) {
+        case 'fetch':
+            return {
+                ...state,
+                loading: true
+            }
+        case 'author':
+            return {
+                ...state,
+                author: action.author
+            }
+        case 'posts':
+            return {
+                ...state,
+                posts: action.posts,
                 loading: false
-            }))
-    }
-
-    render() {
-        const { author, posts, loading } = this.state;
-        const date = author && formatTimestamp(author.created)
-        return (
-            <div>
-                {author && (
-                    <div>
-                        <h2>{author.id}</h2>
-                        <p>Joined {date} has {author.karma} karma</p>
-                        <div dangerouslySetInnerHTML={{ __html: author.about }}></div>
-                    </div>
-                )}
-                {this.state.loading && <div className="spinner"></div>}
-
-                {posts && (
-                    <div>
-                        <h4>Posts</h4>
-                        {posts.map((post) => {
-                            return <ArticleInfo post={post} key={post.id} />
-                        })}
-                    </div>
-                )}
-            </div>
-        );
+            }
+        case 'error':
+            return {
+                ...state,
+                error: error.message,
+                loading: false
+            }
     }
 }
 
-AuthorInfo.propTypes = {
-    location: PropTypes.objectOf(PropTypes.any).isRequired
+
+const AuthorInfo = ({ location }) => {
+    const [state, dispatch] = useReducer(authorReducer,
+        { author: null, posts: [], loading: true })
+
+
+    useEffect(() => {
+        fetchUser(location.state.author)
+            .then(author => {
+                dispatch({ type: 'author', author })
+                return fetchPosts(author.submitted.slice(0, 30))
+            })
+            .then(posts => posts.filter(({ type }) => type === 'story'))
+            .then(posts => dispatch({ type: 'posts', posts }))
+            .catch(error => dispatch({ type: 'error', error }))
+    }, [])
+
+    const { author, posts, loading } = state;
+    const date = author && formatTimestamp(author.created)
+    return (
+        <div>
+            {author && (
+                <div>
+                    <h2>{author.id}</h2>
+                    <p>Joined {date} has {author.karma} karma</p>
+                    <div dangerouslySetInnerHTML={{ __html: author.about }}></div>
+                </div>
+            )}
+            {loading && <div className="spinner"></div>}
+
+            {posts && (
+                <div>
+                    <h4>Posts</h4>
+                    {posts.map((post) => {
+                        return <ArticleInfo post={post} key={post.id} />
+                    })}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default AuthorInfo;
